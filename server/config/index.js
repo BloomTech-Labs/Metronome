d836297234a797c295b4e04358e90e9fe1949076
Ting Wang <felix.wang.1026@gmail.com>
2018-04-30 19:12:19 -0400

    * Fixed syntax errors by ESLint rules

--- server/config/index.js
+++ server/config/index.js
@@ -2,5 +2,5 @@
  * secret for generating json web token
  */
 module.exports = {
-  secret: process.env.NODE_ENV === 'production' ? process.env.SECRET : 'metronome secret'
-}
+  secret: process.env.NODE_ENV === 'production' ? process.env.SECRET : 'metronome secret',
+};
