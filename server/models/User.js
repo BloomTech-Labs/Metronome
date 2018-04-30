25be873919750c88aac4f0f270842c3fb52db250
Ting Wang <felix.wang.1026@gmail.com>
2018-04-30 15:03:27 -0400

    * Added User model to backend * Added setPassword function to set plain password to password hash * Added isValidPassword function to check if password hash is validate * Added generateJWT function to generate jwt for user

---
+++ server/models/User.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { secret } = require('../config');
+const saltRounds = 11;
+const expDay = 30; // expiration time of JWT
+
+mongoose.Promise = global.Promise;
+
+const Schema = mongoose.Schema;
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    lowercase: true,
+    unique: true,
+    required: [true, `can't be blank`],
+    match: [/\S+@\S+\.\S+/, 'is invalid'],
+    index: true,
+  },
+  passwordHash: String,
+}, { timestamps: true });
+
+/**
+ * set plain password to password hash
+ * @param {String} password
+ */
+UserSchema.methods.setPassword = function(password) {
+  this.passwordHash = bcrypt.hashSync(password, saltRounds);
+};
+
+/**
+ * validate the password
+ * @param {String} password
+ */
+UserSchema.methods.isValidPassword = function(password) {
+  return bcrypt.compareSync(password, this.passwordHash);
+};
+
+/**
+ * generate JWT for user
+ */
+UserSchema.methods.generateJWT = function() {
+  const today = new Date();
+  const exp = new Date(today);
+  exp.setDate(today.getDate() + expDay);
+
+  return jwt.sign({
+    id: this._id,
+    email: this.email,
+    exp: parseInt(exp.getTime() / 1000)
+  }, secret);
+};
+
+module.exports = mongoose.model('User', UserSchema);
