49e4197cd49e405cf11e226503f33b213c66b902
Ting Wang <felix.wang.1026@gmail.com>
2018-04-30 16:48:25 -0400

    added heroku deployment requirements

--- server/server.js
+++ server/server.js
@@ -1,7 +1,14 @@
 const app = require('./app');
+const PORT = process.env.PORT || 3000;
+const path = require("path");
 
-const server = app.listen('3000', () => {
-  console.log('App is running at http://localhost:3000');
+// Right before your app.listen(), add this:
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
+const server = app.listen(PORT, () => {
+  console.log(`App is running at http://localhost:${PORT}`);
 });
 
 module.exports = server;
