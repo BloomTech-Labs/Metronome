dcecb8c330c88b35979eb1478f3916d960420a8a
Adam Fetters <adamfetterscoder@gmail.com>
2018-04-30 16:01:47 -0400

    Basic signup page with email, password, confirm password

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,17 +1,14 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
+
+import SignUp from './components/Signup/SignUp';
 
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<h1 className="App-title">Welcome to React</h1>
-				</header>
-				<p className="App-intro">
-					To get started, edit <code>src/App.js</code> and save to reload.
-				</p>
+			<div>
+				<Route path="/signup" component={SignUp} />
 			</div>
 		);
 	}
