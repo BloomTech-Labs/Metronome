479d3c033016b7f69fdcaa9cbe267cfe0987996e
Emma Drueke <emma.drueke@gmaila.com>
2018-05-01 12:38:18 -0500

    Initial attempt at MainImage

--- front-end/src/components/landingpage/landingpage.jsx
+++ front-end/src/components/landingpage/landingpage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import BuyNowButton from './buyNowButton';
 import LoginButtons from './signInSignUp';
+import MainImage from './mainImage';
 
 const LandingPage = () => (
   <div>
     <LoginButtons />
+    <MainImage />
     <BuyNowButton />
   </div>
 );
--- front-end/src/components/landingpage/mainImage.jsx
+++ front-end/src/components/landingpage/mainImage.jsx
@@ -1,1 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Carousel } from 'react-bootstrap';
+
+const MainImage = () => (
+  <div>
+    <Carousel>
+      <Carousel.Item>
+        <img width={900} height={500} alt="900x500" src="/carousel.png" />
+        <Carousel.Caption>
+          <p>Come One</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img width={900} height={500} alt="900x500" src="/carousel.png" />
+        <Carousel.Caption>
+          <p>Come All</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  </div>
+);
+
+export default MainImage;
