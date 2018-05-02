ee13ac8750a02c8c2b9079f17d5ada88c0382bc8
Adam Fetters <adamfetterscoder@gmail.com>
2018-05-02 09:50:22 -0400

    Passing down down through assignment pages

--- front-end/src/components/Assignments/AssignmentDetails.js
+++ front-end/src/components/Assignments/AssignmentDetails.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
-const AssignmentDetails = props => <div>{props.client}</div>;
+const AssignmentDetails = props => (
+  <div>
+    <ul>
+      <li>{props.client}</li>
+      <li>{props.dueDate}</li>
+      <li>{props.music}</li>
+    </ul>
+  </div>
+);
 
 export default AssignmentDetails;
