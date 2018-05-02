fdce3944bc5f81b04ea9c3b78393da433cda530d
Emma Drueke <emma.drueke@gmaila.com>
2018-05-02 15:14:23 -0500

    reordered imports in landingpage.js

--- front-end/src/components/landingpage/landingpage.js
+++ front-end/src/components/landingpage/landingpage.js
@@ -1,8 +1,9 @@
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+
 import React from 'react';
 import BuyNowButton from './buyNowButton';
 import LoginButtons from './signInSignUp';
 import MainImage from './mainImage';
-import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const LandingPage = () => (
   <div>
