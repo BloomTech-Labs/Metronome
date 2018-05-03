8f421a0506296519a69ccd58858331b5272675d6
Emma Drueke <emma.drueke@gmaila.com>
2018-05-03 13:13:04 -0500

    User Settings Form looking hot

--- front-end/src/components/UserSettings/UserSettings.js
+++ front-end/src/components/UserSettings/UserSettings.js
@@ -1,8 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
 import SideMenu from '../SideMenu/SideMenu';
+import { Button } from 'material-ui';
 
-const UserSettings = () => (
-  <SideMenu />
-);
+class UserSettings extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      newEmail: '',
+      oldPassword: '',
+      newPassword: '',
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <form>
+          <h1>Update Email</h1>
+          <div>
+            <input
+              updateEmail=""
+              placeholder="New Email"
+            />
+            <br />
+            <Button
+            variant="raised"
+            color="primary"
+            type="submit"
+            >
+              Submit New Email
+            </Button>
+            </div>
+          <div>
+            <h1>Change Password</h1>
+            <input
+              oldPass=""
+              placeholder="Old Password"
+            />
+            <br />
+          </div>
+          <div>
+            <input
+              newPass=""
+              placeholder="NewPassword"
+            />
+            <br />
+          </div>
+          <Button
+            variant="raised"
+            color="primary"
+            type="submit"
+          >
+            Change Password
+          </Button>
+        </form>
+      </div>
+    )
+  }
+}
 
 export default UserSettings;
