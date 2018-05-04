637d258efa810caad74355ba10d61732c1c57ebc
Ting Wang <felix.wang.1026@gmail.com>
2018-05-04 00:47:49 -0400

    added cors middleware and configure the cors options

--- server/app.js
+++ server/app.js
@@ -1,12 +1,17 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const path = require('path');
 const authRoutes = require('./routes/userRoutes');
 const { isAuthenticated } = require('./services/auth');
 
 const app = express();
-
+const corsOptions = {
+  origin: process.env.NODE_ENV ? '' : 'http://localhost:3000',
+  credentials: true,
+};
+app.use(cors(corsOptions));
 // ... other app.use middleware setups
 app.use(express.static(path.join(__dirname, '../front-end/build')));
 app.use(bodyParser.json());
