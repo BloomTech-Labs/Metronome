903b1caa20bac42a5e7d15f0b0fcc0453b551059
Ting Wang <felix.wang.1026@gmail.com>
2018-05-04 00:06:14 -0400

    added transaction function and send request to backend

--- front-end/src/components/Billing/CheckoutForm.js
+++ front-end/src/components/Billing/CheckoutForm.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { injectStripe } from 'react-stripe-elements';
+import axios from 'axios';
+import jwtDecode from 'jwt-decode';
 import CardSection from './CardSection';
 import { FormGroup, FormControlLabel } from 'material-ui/Form';
 import Checkbox from 'material-ui/Checkbox';
@@ -20,22 +22,41 @@ class CheckoutForm extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
-
+    const url = 'localhost:3000/api/user/transaction';
+    const jwt = window.localStorage.getItem('token') || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVhZWI3Mjk4NTU0NmIwOGJkNDYzYjY0YyIsImVtYWlsIjoiMTIzQDEyMy5jb20iLCJuYW1lIjoidGluZyB3YW5nIiwiZXhwIjoxNTI3OTcxNzM3LCJpYXQiOjE1MjUzNzk3Mzd9.pc5jdU6FYaxiNMnfkW85H2ppAeoo1lcdAt9gcOuOMAQ';
+    const decode = jwtDecode(jwt);
+    const { id } = decode;
     const { subscribe, client } = this.state;
     if (!subscribe && !client) {
       return this.setState({ error: 'Please select a plan' });
     }
     this.setState({ error: null });
 
+    const subscribeType = this.state.subscribe ? '1 Month' :
+      this.state.client ? '1 Client' : false;
+    const price = subscribeType === '1 Month' ? '20' :
+      subscribeType === '1 Client' ? '1.99' : undefined;
 
-    // const subscribeType = this.state.subscribe ? "1 Month" :
-    //                       this.state.client ? "1 Client" : false;
-    // const price = subscribeType === '1 Month' ? '20' :
-    //               subscribeType === '1 Client' ? '1.99' : undefined;
-
-    this.props.stripe.createToken({})
+    this.props.stripe.createToken()
       .then(({ token }) => {
+        // if (!stripeToken) return this.setState({ error: 'Card information incorrect' });
+        this.setState({ error: null });
         console.log('Received Stripe token:', token);
+        axios.post(url, {
+          userId: id,
+          tokenId: token.id,
+          subscribeType,
+          price,
+        }, {
+          headers: {
+            Authorization: jwt,
+          },
+        }).then((res) => {
+          // redirect to user page
+          console.log('Transaction successful', res.data);
+        }).catch((error) => {
+          console.log('Transaction Error', error);
+        });
       })
       .catch((error) => {
         console.log('Error:', error);
