411fba4d0b889c1123df25fe6955f97fe1cf834a
Ting Wang <felix.wang.1026@gmail.com>
2018-05-05 00:06:34 -0400

    added /settings route in the backend

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -7,5 +7,6 @@ routes.post('/login', userController.login);
 routes.put('/', isAuthenticated, userController.editProfile);
 routes.post('/transaction', isAuthenticated, userController.transaction);
 routes.post('/sendmail', userController.sendMail);
+routes.post('/settings', isAuthenticated, userController.editProfile);
 
 module.exports = routes;
