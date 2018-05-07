01d8b12921aa6aa8752f46405a9cffa19e5b11b5
Adam Fetters <adamfetterscoder@gmail.com>
2018-05-07 15:31:13 -0400

    Setup store, redux, redux-promise on app

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import ReduxPromise from 'redux-promise';
+
 import './index.css';
 import App from './App';
 
 import registerServiceWorker from './registerServiceWorker';
 
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root'),
 );
 registerServiceWorker();
