091b94a8e1e482e9dc9cf85abd7e2b5ebefd7650
Adam Fetters <adamfetterscoder@gmail.com>
2018-05-07 15:37:19 -0400

    Took off redux to use another framework/ fixed syntax errors

--- front-end/src/components/Dashboard/InfoDisplay/Assignments/AssignmentCard.js
+++ front-end/src/components/Dashboard/InfoDisplay/Assignments/AssignmentCard.js
@@ -1,39 +1,29 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
-import AssignmentDetails from './AssignmentDetails';
 
 import FATrash from 'react-icons/lib/fa/trash';
 
 import './AssignmentCard.css';
-import {
-	Card,
-	CardImg,
-	CardText,
-	CardBody,
-	CardTitle,
-	Col,
-	Button,
-} from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, Col } from 'reactstrap';
 
 const AssignmentCard = props => (
-	<div>
-		<Col>
-			<Card>
-				<CardTitle>{props.name}</CardTitle>
-				<CardImg
-					top
-					width="20%"
-					src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-					alt="Card image cap"
-				/>
-				<CardBody>
-					<CardText>{props.dueDate}</CardText>
-				</CardBody>
-			</Card>
+  <div>
+    <Col>
+      <Card>
+        <CardTitle>{props.name}</CardTitle>
+        <CardImg
+          top
+          width="20%"
+          src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
+          alt="Card image cap"
+        />
+        <CardBody>
+          <CardText>{props.dueDate}</CardText>
+        </CardBody>
+      </Card>
 
-			<FATrash onClick={() => props.deleteAssignment(props.id)} />
-		</Col>
-	</div>
+      <FATrash onClick={() => props.deleteAssignment(props.id)} />
+    </Col>
+  </div>
 );
 
 export default AssignmentCard;
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,23 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import ReduxPromise from 'redux-promise';
 
 import './index.css';
 import App from './App';
 
 import registerServiceWorker from './registerServiceWorker';
 
-const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
-
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+
   document.getElementById('root'),
 );
 registerServiceWorker();
