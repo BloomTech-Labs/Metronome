5f48f38369d103268e572488384c961c1813917b
Emma Drueke <emma.drueke@gmaila.com>
2018-05-08 12:20:36 -0500

    Testing out Two Cards

--- front-end/src/components/Dashboard/InfoDisplay/StudentAssignments/StudentAssignments.js
+++ front-end/src/components/Dashboard/InfoDisplay/StudentAssignments/StudentAssignments.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Row, Container, Card, CardImg, CardText, CardBody,
+import { Row, Col, Container, Card, CardImg, CardText, CardBody,
   CardTitle, CardSubtitle, Button } from 'reactstrap';
 
 class StudentAssignments extends Component {
@@ -13,16 +13,32 @@ class StudentAssignments extends Component {
   render() {
     return (
       <div>
-        <Card>
-          <CardBody>
-            <CardTitle>Assignment Name</CardTitle>
-            <CardSubtitle>Due Date</CardSubtitle>
-          </CardBody>
-          <img top width="100%" src="http://michaelmarc.com/content/images/thumbs/000/0000410_hallelujah-sheet-music-tabs_400.png" />
-          <CardBody>
-            <CardText>Description of Assignment</CardText>
-          </CardBody>
-        </Card>
+        <Row>
+          <Col sm="6">
+            <Card>
+              <CardBody>
+                <CardTitle>Assignment Name</CardTitle>
+                <CardSubtitle>Due Date</CardSubtitle>
+              </CardBody>
+              <img top width="100%" src="http://michaelmarc.com/content/images/thumbs/000/0000410_hallelujah-sheet-music-tabs_400.png" />
+              <CardBody>
+                <CardText>Description of Assignment</CardText>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col sm="6">
+            <Card>
+              <CardBody>
+                <CardTitle>Assignment Name</CardTitle>
+                <CardSubtitle>Due Date</CardSubtitle>
+              </CardBody>
+              <img top width="100%" src="http://michaelmarc.com/content/images/thumbs/000/0000410_hallelujah-sheet-music-tabs_400.png" />
+              <CardBody>
+                <CardText>Description of Assignment</CardText>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
       </div>
     )
   }
