5873665b1103a358fd8cf70327a9c74e1f605505
Emma Drueke <emma.drueke@gmaila.com>
2018-05-08 11:05:09 -0500

    Created file and basic class for Student Assignments

---
+++ front-end/src/components/Dashboard/InfoDisplay/StudentAssignments/StudentAssignments.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Container, Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button } from 'reactstrap';
+
+class StudentAssignments extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      assignments: [],
+    };
+  }
+
+  redner() {
+    return (
+      <div>
+        <Container>
+          <Row>
+            
+          </Row>
+        </Container>
+      </div>
+    )
+  }
+}
