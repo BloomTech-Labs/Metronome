760be584bd1a2b149ef6bb22b4e49622564c6dda
Jesse Hood <jessenoob@gmail.com>
2018-05-09 11:37:07 -0400

    basic assignment token email working (will still need heavy modification)

--- server/controllers/teacherController.js
+++ server/controllers/teacherController.js
@@ -1,11 +1,13 @@
 const { sendEmail } = require('../services/email');
+const Teacher = require('../models/Teacher');
+const Assignment = require('../models/Assignment');
 
 // Sample email template - change this when we decide on what exactly to send
 const emailTemplate = {
   from: 'lambda.metronome@gmail.com',
   subject: 'Metronome assignment',
   text: 'You have a new Metronome assignment',
-  html: '<h1>Here\'s your assignment</h1>',
+  html: '<p>A teacher has sent you an assignment. </p>',
 };
 
 /**
@@ -34,17 +36,16 @@ exports.emailAssignments = async function (req, res) {
   // https://github.com/sendgrid/sendgrid-nodejs
   // TODO: Add in actual assignments field
   try {
-    if (!req.body.emails || typeof req.body.emails !== 'string') throw new Error();
-    const emails = req.body.emails.split(',').map(email => email.trim());
+    const { emails, name, days, dueDate, hours, musicSheetAddr } = req.body;
+    const emailsArray = req.body.emails.split(',').map(email => email.trim());
+    const teacher = await Teacher.findById(req.user._id);
 
-    const emailsToSend = emails.map((email) => {
-      const template = { to: email, ...emailTemplate };
-      return sendEmail(template);
-    });
-
-    await Promise.all(emailsToSend);
+    const assignment = new Assignment({ emails, name, days, dueDate, hours, musicSheetAddr, teacher: teacher._id });
+    await assignment.save();
+    await teacher.emailAssignment(emailsArray, assignment._id);
     res.status(200).json({ message: 'Emails sent successfully!' });
   } catch (err) {
+    console.log(err);
     res.status(400).json({
       error: 'Could not send emails. Please verify that all email addresses are valid.',
     });
--- server/models/Assignment.js
+++ server/models/Assignment.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const moment = require('moment');
+
+const VALID_DAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 const AssignmentSchema = new mongoose.Schema({
   name: {
@@ -9,6 +12,10 @@ const AssignmentSchema = new mongoose.Schema({
   },
   days: {
     type: [String],
+    required: [true, 'can\'t be blank'],
+  },
+  dueDate: {
+    type: Date,
     required: [true, 'can\'t be blank'],
   },
   hours: {
@@ -24,7 +31,23 @@ const AssignmentSchema = new mongoose.Schema({
     ref: 'Teacher',
   },
   students: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Student' }],
+  emails: [String],
 }, { timestamps: true });
+
+AssignmentSchema.statics.validateDays = function (days = ['']) {
+  const isYYYYMMDD = date => moment(date, 'YYYY-MM-DD', true).isValid();
+  const isBeforeToday = date => moment(date).isBefore(moment(), 'day');
+
+  days.forEach((day) => {
+    if (!isYYYYMMDD(day)) throw new Error(`Invalid date: ${day}. (Must be YYYY-MM-DD format)`);
+    if (isBeforeToday(day)) throw new Error('An assignment date cannot be before today.');
+  });
+};
+
+AssignmentSchema.statics.validateHours = function (hours = 0) {
+  if (Number.isNaN(hours)) throw new Error('Assignment hours must be a number.');
+  if (hours <= 0) throw new Error('Assignment hours must be above 0 hours.');
+};
 
 module.exports = mongoose.model('Assignment', AssignmentSchema);
 
