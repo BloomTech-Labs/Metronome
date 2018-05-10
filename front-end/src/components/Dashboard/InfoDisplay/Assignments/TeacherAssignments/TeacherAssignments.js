87c9fe14394754f22e174e8d9c31b8820c3bd42b
Adam Fetters <adamfetterscoder@gmail.com>
2018-05-09 22:25:36 -0400

    Displaying Student List Assignment Details

--- front-end/src/components/Dashboard/InfoDisplay/Assignments/TeacherAssignments/TeacherAssignments.js
+++ front-end/src/components/Dashboard/InfoDisplay/Assignments/TeacherAssignments/TeacherAssignments.js
@@ -1,32 +1,46 @@
 import React from 'react';
 import {connect} from 'react-redux';
 
+import './teacher-assignments.css';
+
 const TeacherAssignments = props => {
   const {assignmentName} = props.match.params;
   const sortedList = props.assignments.filter (
     assignment => assignment.assignmentName === assignmentName
-  );
-  const clientNames = sortedList.map ((assign) => {
-    return <li>{assign.clientName}</li>;
-  });
-  const daysPractice = sortedList.map((a => {
-   return a.daysToPractice.map((day => {
-      return <li>{day}</li>
-    }))
-  }))
-  
-  return (
-    <div>
-      <h1>{assignmentName}</h1>
+  ); console.log(sortedList);
 
-      <ul>
-        {clientNames}
-        {daysPractice}
-      </ul>
-    </div>
-  );
-};
+  return (
+  sortedList.map ((assign) => (
+    <div>
+    <li>{assign.clientName}</li>
+      <ul>{assign.daysToPractice.map((day) => (
+        <li className='day'>{day}</li>
+      )
+        
+      )}</ul>
+      </div>
+    
+  ))
+)
+}
+  // const daysPractice = sortedList.map((a) => {
+  //  return a.daysToPractice.map((day) => {
+  //     return <li>{day}</li>
+  //   });
+  // });
+  
+  // return (
+  //   <div>
+  //     <h1>{assignmentName}</h1>
+
+  //     {/* // <ul>
+  //     //   {clientNames}
+  //     //   {daysPractice}
+  //     // </ul> */}
+  //   </div>
+  // );
+
 
 const mapStateToProps = state => ({assignments: state.assignments});
 
-export default connect (mapStateToProps, null) (TeacherAssignments);
+export default connect(mapStateToProps, null)(TeacherAssignments);
