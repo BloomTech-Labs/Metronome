897498d43db5838d8544d3fe22928a2b9bfe1060
Adam Fetters <adamfetterscoder@gmail.com>
2018-05-10 11:10:51 -0400

    Working Teacher Assignments page by Assignment name

--- front-end/src/components/Dashboard/InfoDisplay/Assignments/TeacherAssignments/TeacherAssignments.js
+++ front-end/src/components/Dashboard/InfoDisplay/Assignments/TeacherAssignments/TeacherAssignments.js
@@ -1,47 +1,31 @@
 import React from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 
 import './teacher-assignments.css';
 
-const TeacherAssignments = props => {
-  const {assignmentName} = props.match.params;
-  const sortedList = props.assignments.filter (
-    assignment => assignment.assignmentName === assignmentName
-  ); console.log(sortedList);
+const TeacherAssignments = (props) => {
+  const { assignmentName } = props.match.params;
+  const sortedList = props.assignments.filter(assignment => assignment.assignmentName === assignmentName);
 
   return (
-  sortedList.map ((assign) => (
     <div>
       <h1>{assignmentName}</h1>
-    <li>{assign.clientName}</li>  
-      <ul>{assign.daysToPractice.map((day) => (
-        <li className='day'>{day}</li>
-      )
-        
-      )}</ul>
-      </div>
-    
-  ))
-)
-}
-  // const daysPractice = sortedList.map((a) => {
-  //  return a.daysToPractice.map((day) => {
-  //     return <li>{day}</li>
-  //   });
-  // });
-  
-  // return (
-  //   <div>
-  //     <h1>{assignmentName}</h1>
+      {sortedList.map(assign => (
+        <div>
+          <li>{assign.clientName}</li>
+          <ul>{assign.daysToPractice.map(day => (
+            <li className="day">{day}</li>
+      ))}
+          </ul>
+        </div>
 
-  //     {/* // <ul>
-  //     //   {clientNames}
-  //     //   {daysPractice}
-  //     // </ul> */}
-  //   </div>
-  // );
+    ))
+  }
+    </div>
+  );
+};
 
 
-const mapStateToProps = state => ({assignments: state.assignments});
+const mapStateToProps = state => ({ assignments: state.assignments });
 
 export default connect(mapStateToProps, null)(TeacherAssignments);
