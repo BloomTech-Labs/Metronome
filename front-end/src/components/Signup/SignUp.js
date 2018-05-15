fe080a5ca0e1bf562f0defd51cc783dbe731ae56
Emma Drueke <emma.drueke@gmaila.com>
2018-05-15 10:32:22 -0500

    trying to match login page

--- front-end/src/components/Signup/SignUp.js
+++ front-end/src/components/Signup/SignUp.js
@@ -8,6 +8,7 @@ import isEmail from 'validator/lib/isEmail';
 import equals from 'validator/lib/equals';
 import Error from '../Error/Error';
 
+import '../landingpage/landingpage.css';
 import './sign-up.css';
 
 // Input Valiations match backend
@@ -77,79 +78,101 @@ class SignUp extends Component {
 
 	render() {
 	  return (
-  <div className="form-container" style={{marginTop: "-10%"}}>
-    <div className="signup-form">
-      <form onSubmit={this.props.handleSubmit(this.handleFormSubmit)}>
-        <h2 className="title header-con">Sign Up</h2>
-        <Error error={this.props.auth.error} />
-        <div>
-          <Field name="role" component="select">
-            <option>Please select role</option>
-            <option value="Teacher">Teacher</option>
-            <option value="Student">Student</option>
-          </Field>
-        </div>
-        <div className="pair">
-          <Field
-            name="firstName"
-            component="input"
-            placeholder="First Name"
-            autoComplete="off"
-          />
-          <br />
-        </div>
-        <div className="pair">
-          <Field
-            name="lastName"
-            placeholder="Last Name"
-            component="input"
-            autoComplete="off"
-          />
-          <br />
-        </div>
-        <div className="pair">
-          <Field
-            name="email"
-            placeholder="email"
-            type="email"
-            component="input"
-            autoComplete="off"
-          />
-          <br />
-        </div>
-        <div className="pair">
-          <Field
-            name="password"
-            type="password"
-            placeholder="password"
-            component="input"
-            autoComplete="off"
-          />
-          <br />
-        </div>
-        <div className="pair">
-          <Field
-            name="confirmPassword"
-            type="password"
-            placeholder="confirm password"
-            component="input"
-            autoComplete="off"
-          />
-          <br />
-        </div>
-        <Button
-          style={{ fontSize: '14px' }}
-          className="btn--signup"
-          variant="raised"
-          color="primary"
-          type="submit"
-        >Sign Up
-        </Button>
-        <br />
-        <Link className="link" to="/login">Already have an account?</Link>
-      </form>
-    </div>
-  </div>
+      <section id="about" class="container">
+      <div class="header-con text-center" >
+        <div class="row">
+            <div class="col-md-4 col-sm-4">
+                <div class="icon-box">
+                    <div class="icon-box-content">
+                        <div class="icons ">
+                            <i class="fa fa-calendar"></i>
+                        </div>
+                        <div class="content">
+                          <h2 class="title">Sign Up</h2>
+                          <div className="form-container" style={{marginTop: "-10%"}}>
+                            <div className="signup-form">
+                              <form onSubmit={this.props.handleSubmit(this.handleFormSubmit)}>
+                                <h2 className="title header-con">Sign Up</h2>
+                                <Error error={this.props.auth.error} />
+                                <div>
+                                  <Field name="role" component="select">
+                                    <option>Please select role</option>
+                                    <option value="Teacher">Teacher</option>
+                                    <option value="Student">Student</option>
+                                  </Field>
+                                </div>
+                                <div className="pair">
+                                  <Field
+                                    name="firstName"
+                                    component="input"
+                                    placeholder="First Name"
+                                    autoComplete="off"
+                                  />
+                                  <br />
+                                </div>
+                                <div className="pair">
+                                  <Field
+                                    name="lastName"
+                                    placeholder="Last Name"
+                                    component="input"
+                                    autoComplete="off"
+                                  />
+                                  <br />
+                                </div>
+                                <div className="pair">
+                                  <Field
+                                    name="email"
+                                    placeholder="email"
+                                    type="email"
+                                    component="input"
+                                    autoComplete="off"
+                                  />
+                                  <br />
+                                </div>
+                                <div className="pair">
+                                  <Field
+                                    name="password"
+                                    type="password"
+                                    placeholder="password"
+                                    component="input"
+                                    autoComplete="off"
+                                  />
+                                  <br />
+                                </div>
+                                <div className="pair">
+                                  <Field
+                                    name="confirmPassword"
+                                    type="password"
+                                    placeholder="confirm password"
+                                    component="input"
+                                    autoComplete="off"
+                                  />
+                                  <br />
+                                </div>
+                                <Button
+                                  style={{ fontSize: '14px' }}
+                                  className="btn--signup"
+                                  variant="raised"
+                                  color="primary"
+                                  type="submit"
+                                >Sign Up
+                                </Button>
+                                <br />
+                                <Link className="link" to="/login">Already have an account?</Link>
+                              </form>
+                            </div>
+                          </div>
+                          
+             <a href="#" class="btn btn-pri">Read More</a>
+                       </div>
+                      
+                   </div>
+               </div>
+           </div>
+           </div>
+           </div>
+           </section>
+
 	  );
 	}
 }
