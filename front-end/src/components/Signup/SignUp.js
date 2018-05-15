6a12a665f432c4fbef96b8124475fb55c5cee39c
Emma Drueke <emma.drueke@gmaila.com>
2018-05-15 12:55:53 -0500

    Attempt one for a background image

--- front-end/src/components/Signup/SignUp.js
+++ front-end/src/components/Signup/SignUp.js
@@ -7,6 +7,7 @@ import Button from 'material-ui/Button';
 import isEmail from 'validator/lib/isEmail';
 import equals from 'validator/lib/equals';
 import Error from '../Error/Error';
+import Background from '../landingpage/assets/background.jpg';
 
 
 import './sign-up.css';
