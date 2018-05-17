248e7ed14a8317b437bb6066727ba670061c725c
Ting Wang <felix.wang.1026@gmail.com>
2018-05-16 20:53:09 -0400

    fixed the assignments in addassignment page to go back to assignments page

--- front-end/src/components/Dashboard/InfoDisplay/Assignments/AddAssignments/AssignmentForm.js
+++ front-end/src/components/Dashboard/InfoDisplay/Assignments/AddAssignments/AssignmentForm.js
@@ -1,21 +1,21 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import DatePicker from 'react-datepicker';
 import moment from 'moment';
-import {Grid, Checkbox, Button} from 'material-ui';
+import { Grid, Checkbox, Button } from 'material-ui';
 import Dropzone from 'react-dropzone';
 import axios from 'axios';
 import jwtDecode from 'jwt-decode';
 import 'react-datepicker/dist/react-datepicker.css';
-import {addAssignment} from '../../../../../actions';
+import { addAssignment } from '../../../../../actions';
 import './assignment-form.css';
 
 axios.defaults.withCredentials = true;
 
 class AssignmentForm extends Component {
-  constructor (props) {
-    super (props);
+  constructor(props) {
+    super(props);
 
     this.state = {
       name: '',
@@ -29,7 +29,7 @@ class AssignmentForm extends Component {
       Friday: false,
       Saturday: false,
       dueDate: '',
-      date: moment (),
+      date: moment(),
       musicSheetAddr: '',
       email: '',
       fileName: '',
@@ -39,8 +39,8 @@ class AssignmentForm extends Component {
   }
 
   // Changes state of items that are handled
-  handleStateDataChange = event => {
-    this.setState ({
+  handleStateDataChange = (event) => {
+    this.setState({
       [event.target.name]: event.target.value,
     });
   };
@@ -48,23 +48,23 @@ class AssignmentForm extends Component {
   // Handles the unchecking and checking of days to practice
   // adds or takes away from array based on state of checked
   // this is what gets passed up to assignments
-  handleChange = name => event => {
-    this.setState ({
+  handleChange = name => (event) => {
+    this.setState({
       [name]: event.target.checked,
     });
     if (event.target.checked) {
-      this.setState ({
-        days: {...this.state.days, [name]: false},
+      this.setState({
+        days: { ...this.state.days, [name]: false },
       });
     }
   };
 
   //	handles date of the date picker
 
-  handledDateChange = date => {
-    const dueDate = date.format ('l');
+  handledDateChange = (date) => {
+    const dueDate = date.format('l');
 
-    this.setState ({
+    this.setState({
       dueDate,
       date,
     });
@@ -72,27 +72,27 @@ class AssignmentForm extends Component {
 
   // This is for handling filed upload
   // TODO: Need to make sure this works with server figure out how to send data
-  onDrop = files => {
-    const formData = new FormData ();
-    formData.append ('file', files[0]);
+  onDrop = (files) => {
+    const formData = new FormData();
+    formData.append('file', files[0]);
 
     axios
-      .post ('/api/teacher/getUploadUrl', formData, {
+      .post('/api/teacher/getUploadUrl', formData, {
         headers: {
-          Authorization: localStorage.getItem ('token'),
+          Authorization: localStorage.getItem('token'),
           'Content-Type': 'multipart/form-data',
         },
       })
-      .then (response => {
-        const {fileName, musicSheetAddr} = response.data;
-        this.setState ({
+      .then((response) => {
+        const { fileName, musicSheetAddr } = response.data;
+        this.setState({
           preview: files[0].preview,
           fileName,
           musicSheetAddr,
           disabled: true,
         });
       })
-      .catch (err => console.log (err));
+      .catch(err => console.log(err));
   };
 
   addAssignment = () => {
@@ -107,7 +107,7 @@ class AssignmentForm extends Component {
       fileName,
     } = this.state;
 
-    const emails = email.split (',');
+    const emails = email.split(',');
     const assignment = {
       emails,
       name,
@@ -117,8 +117,8 @@ class AssignmentForm extends Component {
       musicSheetAddr,
       fileName,
     };
-    this.props.addAssignment (assignment);
-    this.setState ({
+    this.props.addAssignment(assignment);
+    this.setState({
       name: '',
       days: {},
       hours: '',
@@ -147,7 +147,7 @@ class AssignmentForm extends Component {
     return (
       <div>
         <div className="assignment-form-container">
-          <div style={{margin: 40 }}>
+          <div style={{ margin: 40 }}>
             <Grid container spacing={0} align="center">
               <Grid item xs={12}>
                 <input
@@ -162,49 +162,49 @@ class AssignmentForm extends Component {
                     <Checkbox
                       className="daysToPractice"
                       checked={this.state.Sunday}
-                      onChange={this.handleChange ('Sunday')}
+                      onChange={this.handleChange('Sunday')}
                       value="checkedSunday"
                     />
                     <label htmlFor="checkedSunday">Sunday</label>
                     <Checkbox
                       className="daysToPractice"
                       checked={this.state.Monday}
-                      onChange={this.handleChange ('Monday')}
+                      onChange={this.handleChange('Monday')}
                       value="checkedMonday"
                     />
                     <label htmlFor="checkedMonday">Monday</label>
                     <Checkbox
                       className="daysToPractice"
                       checked={this.state.Tuesday}
-                      onChange={this.handleChange ('Tuesday')}
+                      onChange={this.handleChange('Tuesday')}
                       value="checkedTuesday"
                     />
                     <label htmlFor="checkedTuesday">Tuesday</label>
                     <Checkbox
                       className="daysToPractice"
                       checked={this.state.Wednesday}
-                      onChange={this.handleChange ('Wednesday')}
+                      onChange={this.handleChange('Wednesday')}
                       value="checkedWednesday"
                     />
                     <label htmlFor="checkedWednesday">Wednesday</label>
                     <Checkbox
                       className="daysToPractice"
                       checked={this.state.Thursday}
-                      onChange={this.handleChange ('Thursday')}
+                      onChange={this.handleChange('Thursday')}
                       value="checkedThursday"
                     />
                     <label htmlFor="checkedThursday">Thursday</label>
                     <Checkbox
                       className="daysToPractice"
                       checked={this.state.Friday}
-                      onChange={this.handleChange ('Friday')}
+                      onChange={this.handleChange('Friday')}
                       value="checkedFriday"
                     />
                     <label htmlFor="checkedFriday">Friday</label>
                     <Checkbox
                       className="daysToPractice"
                       checked={this.state.Saturday}
-                      onChange={this.handleChange ('Saturday')}
+                      onChange={this.handleChange('Saturday')}
                       value="checkedSaturday"
                     />
                     <label htmlFor="checkedSaturday">Saturday</label>
@@ -280,7 +280,7 @@ class AssignmentForm extends Component {
                   <Grid item>
                     <Button
                       variant="raised"
-                      onClick={this.props.history.goBack}
+                      onClick={() => this.props.history.push('/dashboard/assignments')}
                     >
                       Assignments
                     </Button>
@@ -299,4 +299,4 @@ AssignmentForm.propTypes = {
   addAssignment: PropTypes.func.isRequired,
 };
 
-export default connect (null, {addAssignment}) (AssignmentForm);
+export default connect(null, { addAssignment })(AssignmentForm);
