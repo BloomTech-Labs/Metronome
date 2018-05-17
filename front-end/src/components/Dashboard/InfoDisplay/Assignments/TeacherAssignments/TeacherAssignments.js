330400cdb9ca09606765997cc331dfa8f42e72b3
Ting Wang <felix.wang.1026@gmail.com>
2018-05-17 13:00:41 -0400

    added unclaim lists to teacherassighments

--- front-end/src/components/Dashboard/InfoDisplay/Assignments/TeacherAssignments/TeacherAssignments.js
+++ front-end/src/components/Dashboard/InfoDisplay/Assignments/TeacherAssignments/TeacherAssignments.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Checkbox } from 'material-ui';
+import { Checkbox, Button } from 'material-ui';
+import { Link } from 'react-router-dom';
 import './teacher-assignments.css';
 import { getAssignmentById } from '../../../../../actions';
 
@@ -11,26 +12,36 @@ class TeacherAssignments extends Component {
 
   render() {
     if (!this.props.assignment.assignment) { return <div>Loading...</div>; }
+    console.log(this.props.assignment);
     return (
       <div>
         <h1>{this.props.assignment.assignment.name}</h1>
-        <div>{this.props.assignment.assignment.students.map(student => (
-          <div>
-            <h3>{`Student Name: ${student.firstName} ${student.lastName}`}</h3>
-            <div style={{ display: 'flex' }}>{Object.keys(this.props.assignment.assignment.days).map(day => (
-              <div>
-                <Checkbox
-                  name={day}
-                  checked={student.progress[day]}
-                  disabled
-                />
-                <label htmlFor={day}>{day}</label>
+        {this.props.assignment.assignment.students.length !== 0 ?
+          <ul className="list-group">{this.props.assignment.assignment.students.map(student => (
+            <li className="list-group-item" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+              <h3>Student:&nbsp;</h3>
+              <h4>{`${student.firstName} ${student.lastName}`}</h4>
+              <div style={{ display: 'flex' }}>{Object.keys(this.props.assignment.assignment.days).map(day => (
+                <div>
+                  <Checkbox
+                    name={day}
+                    checked={student.progress[day]}
+                    disabled
+                  />
+                  <label htmlFor={day}>{day}</label>
+                </div>
+                    ))}
               </div>
-                  ))}
-            </div>
-          </div>
-            ))}
-        </div>
+            </li>
+              ))}
+          </ul>
+          : <ul className="list-group">{this.props.assignment.assignment.emails.map(email => <li className="list-group-item">{email} <span style={{ color: 'red' }}>Unclaimed</span></li>)}</ul>
+        }
+        <Link to="/dashboard/assignments">
+          <Button variant="raised">
+            Assignments
+          </Button>
+        </Link>
       </div>
     );
   }
