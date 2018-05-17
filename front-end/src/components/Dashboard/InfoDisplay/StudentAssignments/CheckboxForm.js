b2c718cb4b1fdb8e79688da51c426ef5a16cfbb7
Adam Fetters <adamfetterscoder@gmail.com>
2018-05-17 09:57:09 -0400

    Added a toast info box on student update assignment

--- front-end/src/components/Dashboard/InfoDisplay/StudentAssignments/CheckboxForm.js
+++ front-end/src/components/Dashboard/InfoDisplay/StudentAssignments/CheckboxForm.js
@@ -1,36 +1,45 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { connect } from 'react-redux';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { updateAssignment } from '../../../../actions';
 
 class CheckboxForm extends Component {
   handleFormSubmit = (progress) => {
     this.props.updateAssignment(progress, this.props.assignmentId);
+    this.notify();
   };
+
+  notify = () => toast.info('Assignment Updated!', { autoClose: 5000, position: toast.POSITION.TOP_LEFT });
 
   render() {
     const { days } = this.props;
     return (
-      <form onSubmit={this.props.handleSubmit(this.handleFormSubmit)}>
-        <fieldset>
-          <legend>Check off when you practice</legend>
-          {Object.keys(days).map(day => (
-            <div className="day-check-container">
-              <div className="box">
-                <label htmlFor={day}>{day}</label>
-                <Field
-                  className="check-box"
-                  name={day}
-                  label={day}
-                  type="checkbox"
-                  component="input"
-                />
+      <div>
+
+        <form onSubmit={this.props.handleSubmit(this.handleFormSubmit)}>
+          <fieldset>
+            <legend>Check off when you practice</legend>
+            {Object.keys(days).map(day => (
+              <div className="day-check-container">
+                <div className="box">
+                  <label htmlFor={day}>{day}</label>
+                  <Field
+                    className="check-box"
+                    name={day}
+                    label={day}
+                    type="checkbox"
+                    component="input"
+                  />
+                </div>
               </div>
-            </div>
               ))}
-        </fieldset>
-        <button style={{ width: 'calc(100% - 30px)' }}>update</button>
-      </form>
+          </fieldset>
+          <button style={{ width: 'calc(100% - 30px)' }}>update</button>
+          <ToastContainer autoClose={5000} />
+        </form>
+      </div>
     );
   }
 }
